82e0fad4fa6443173f81919ae75e50468e0ead3a
naoki <hinnm483@gmail.com>
2021-10-10 22:02:23 +0900

    licenseブレードの切り分け

--- src/resources/views/components/navbar.blade.php
+++ src/resources/views/components/navbar.blade.php
@@ -1,10 +1,3 @@
-{{--
-    Copyright: Material Design for Bootstrap
-    https://mdbootstrap.com/
-
-    Read the license: https://mdbootstrap.com/general/license/
---}}
-
 <!-- ナビバー -->
 <nav
     id="main-navbar"
--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    @include('components.license')
     @include('components.sidebar')
     @include('components.navbar')
 @endsection
