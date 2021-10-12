683ca844345a91d54a1f1a3a74b7caadfbc57a42
naoki <hinnm483@gmail.com>
2021-10-12 19:06:48 +0900

    ナビバーの共通化とHTML構造修正

--- src/resources/views/components/sidebar.blade.php
+++ src/resources/views/components/sidebar.blade.php
@@ -3,21 +3,19 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 @endpush
 
-<header>
-    <!-- サイドバー -->
-    <nav
-        {{-- トグルボタンとの紐付け --}}
-        id="sidebarMenu"
-        {{-- "画面がLGサイズ以上" or "トグルボタンをクリック" でサイドバー表示 --}}
-        class="collapse d-lg-block sidebar collapse bg-white"
-    >
-        <!-- サイドに固定 -->
-        <div class="position-sticky">
-            <div class="list-group list-group-flush mx-3 mt-4">
-                {{-- サイドバーのリスト --}}
-                {{ $sidebar_list }}
-            </div>
+<!-- サイドバー -->
+<nav
+    {{-- トグルボタンとの紐付け --}}
+    id="sidebarMenu"
+    {{-- "画面がLGサイズ以上" or "トグルボタンをクリック" でサイドバー表示 --}}
+    class="collapse d-lg-block sidebar collapse bg-white"
+>
+    <!-- サイドに固定 -->
+    <div class="position-sticky">
+        <div class="list-group list-group-flush mx-3 mt-4">
+            {{-- サイドバーのリスト --}}
+            {{ $sidebar_list }}
         </div>
-    </nav>
-    <!-- サイドバー -->
-</header>
+    </div>
+</nav>
+<!-- サイドバー -->
