03af5254a6ed0709b11998f1e1c0acac6112bcbc
naoki <hinnm483@gmail.com>
2021-10-19 14:08:21 +0900

    不要Vue.componentの削除とcomponentディレクトリ構成の変更

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -20,8 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('avatar-image-change', require('./components/AvatarImageChange.vue').default);
+Vue.component('avatar-image-change', require('./components/users/AvatarImageChange.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
