86da8577a3748f846a420911e55cfdc691372c14
naoki <hinnm483@gmail.com>
2021-11-03 23:22:16 +0900

    laravel-コンポーネントの切り分け/#56

---
+++ src/resources/views/components/graphs/axis_set_nav.blade.php
@@ -0,0 +1,154 @@
+<div class="mt-4">
+    <div class="mb-3 btn rounded-pill btn-block"
+        :class="{'btn-primary': isActiveSetAxisX}"
+        v-on:click="isActiveSetAxisX = true; isActiveSetAxisY = false">
+        横(x)軸を設定する
+    </div>
+
+    <div class="btn rounded-pill btn-block"
+        :class="{'btn-primary': isActiveSetAxisY}"
+        v-on:click="isActiveSetAxisY = true; isActiveSetAxisX = false">
+        縦(y)軸を設定する
+    </div>
+</div>
+
+<div class="axis-setting-nav"
+    v-show="isActiveSetAxisX">
+    <ol class="mt-5 pl-3 mb-2">
+        <li>グラフ上で横軸の最小値、最大値を順にクリックしてください。</li>
+
+        <li class="mt-3">
+            <p class="mb-2">軸の値を下記に入力してください。</p>
+            {{-- 数値の入力フォーム --}}
+            <div class="row" style="height: 50px">
+                <div class="col pr-2">
+                    <div class="md-form mt-0">
+
+                    <label class="form-label ml-1 mb-1" for="x_axis_min">
+                        x min
+                    </label>
+
+                    <input
+                        type="number"
+                        id="x_axis_min"
+                        name="x_axis_min"
+                        class="form-control @error('x_axis_min') is-invalid @enderror"
+                        autocomplete="off"
+                        value="{{ old('x_axis_min') }}"
+                        required
+                    >
+                    @error('x_axis_min')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                </div>
+
+                <div class="col pl-2">
+                    <div class="md-form mt-0">
+
+                    <label class="form-label ml-1 mb-1" for="x_axis_max">
+                        x max
+                    </label>
+
+                    <input
+                        type="number"
+                        id="x_axis_max"
+                        name="x_axis_max"
+                        class="form-control @error('x_axis_max') is-invalid @enderror"
+                        autocomplete="off"
+                        value="{{ old('x_axis_max') }}"
+                        required
+                    >
+                    @error('x_axis_max')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ol>
+
+    <div class="text-right mt-3 mr-1">
+        <div class="btn btn-link mr-0 pr-0">
+            リセット
+        </div>
+        <div class="btn btn-link" style="color: #3490dc">
+            <strong>OK</strong>
+        </div>
+    </div>
+</div>
+
+<div class="axis-setting-nav"
+    v-show="isActiveSetAxisY">
+    <ol class="mt-5 pl-3 mb-2">
+        <li>グラフ上で縦軸の最小値、最大値を順にクリックしてください。</li>
+
+        <li class="mt-3">
+            <p class="mb-2">軸の値を下記に入力してください。</p>
+            {{-- 数値の入力フォーム --}}
+            <div class="row" style="height: 50px">
+                <div class="col pr-2">
+                    <div class="md-form mt-0">
+
+                    <label class="form-label ml-1 mb-1" for="y_axis_min">
+                        y min
+                    </label>
+
+                    <input
+                        type="number"
+                        id="y_axis_min"
+                        name="y_axis_min"
+                        class="form-control @error('y_axis_min') is-invalid @enderror"
+                        autocomplete="off"
+                        value="{{ old('y_axis_min') }}"
+                        required
+                    >
+                    @error('y_axis_min')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                </div>
+
+                <div class="col pl-2">
+                    <div class="md-form mt-0">
+
+                    <label class="form-label ml-1 mb-1" for="y_axis_max">
+                        y max
+                    </label>
+
+                    <input
+                        type="number"
+                        id="y_axis_max"
+                        name="y_axis_max"
+                        class="form-control @error('y_axis_max') is-invalid @enderror"
+                        autocomplete="off"
+                        value="{{ old('y_axis_max') }}"
+                        required
+                    >
+                    @error('y_axis_max')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ol>
+
+    <div class="text-right mt-3 mr-1">
+        <div class="btn btn-link mr-0 pr-0">
+            リセット
+        </div>
+        <div class="btn btn-link" style="color: #3490dc">
+            <strong>OK</strong>
+        </div>
+    </div>
+</div>
+</div>
