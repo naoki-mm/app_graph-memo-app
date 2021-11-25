8c021aa70514e412dbcdc880bf15350cf9281333
naoki <hinnm483@gmail.com>
2021-11-26 01:08:52 +0900

    お気に入りアイコンのUI調整/#82

--- src/resources/js/components/graphs/GraphFavorite.vue
+++ src/resources/js/components/graphs/GraphFavorite.vue
@@ -1,7 +1,7 @@
 <template>
     <button type="button" class="btn m-0 p-1 shadow-none">
         <i class="fa-star"
-            :class="{far:!isFavorite, fas:isFavorite, 'yellow-text':isFavorite}"
+            :class="favoriteClass"
             @click="updateFavorite()"
         >
         </i>
@@ -21,12 +21,28 @@ export default {
     data() {
         return {
             isFavorite: this.initialIsFavorite,
+            favoriteClass: {
+                far: !this.initialIsFavorite,
+                fas: this.initialIsFavorite,
+                'favorite-star': this.initialIsFavorite,
+                animated: this.initialIsFavorite,
+                flip: this.initialIsFavorite,
+                fast: this.initialIsFavorite,
+            },
         };
     },
 
     methods: {
         updateFavorite() {
-            this.isFavorite  = !this.isFavorite;
+            // お気に入りアイコンのクラスを切り替え
+            Object.keys(this.favoriteClass).forEach((className) => {
+                this.favoriteClass[className] = !this.favoriteClass[className];
+            })
+
+            // お気に入りのアクティブ状態の切り替え
+            this.isFavorite = !this.isFavorite;
+
+            // お気に入りの状態を非同期通信
             let booleanIsFavorite = Number(this.isFavorite);
             axios.put(this.favoriteUpdateEndpoint, {'favorite': booleanIsFavorite});
         }
@@ -35,5 +51,9 @@ export default {
 </script>
 
 <style>
-
+/* お気に入りアイコンのスタイル */
+.favorite-star {
+    color: rgb(255, 230, 0);
+    text-shadow: 0 0 2px #000;
+}
 </style>
