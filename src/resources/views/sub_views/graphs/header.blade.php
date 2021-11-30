11c6d55818147c59ac98d31c60bdcf802424c9ba
naoki <hinnm483@gmail.com>
2021-12-01 05:40:00 +0900

    サイドバーのデザイン変更

--- src/resources/views/sub_views/graphs/header.blade.php
+++ src/resources/views/sub_views/graphs/header.blade.php
@@ -12,7 +12,7 @@
                         {{-- メモ一覧遷移リンク --}}
                         <li class="list-inline-item">
                             <a href="{{ route('graph.index') }}" class="list-group-item-action">
-                                <i class="fas fas fa-home fa-fw me-3 mr-2"></i><span>プロットメモ</span>
+                                <i class="fas fa-sticky-note me-3 mr-2"></i></i></i><span>プロットメモ</span>
                             </a>
                         </li>
                         {{-- トグルボタン --}}
@@ -29,12 +29,15 @@
 
         <div class="list-group-item py-0" aria-current="true">
             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                <div class="my-1">
+                <div class="pl-2 my-1 navbar-sub-contents">
+                    <p class="mb-0 pl-2"><i class="bi bi-star-fill mr-1"></i>お気に入り</p>
+                    <hr class="my-1">
+                    <p class="mb-1 pl-2"><i class="bi bi-tags-fill mr-1"></i>タグ一覧</p>
                     {{-- タグ一覧表示 --}}
                     @isset($all_tags)
                         @foreach($all_tags as $tag_array)
                             @if($loop->first)
-                                <div class="pb-4 pl-3">
+                                <div class="pb-2 pl-3">
                                 @endif
                                     <a href="{{ route('tag.index', ['name' => $tag_array['text']]) }}" class="d-inline-block badge badge-light p-1 mr-1 mt-1">
                                         {{ $tag_array['text'] ?? [] }}
@@ -49,6 +52,10 @@
                             {{ $tag->graphs->count() }}件
                         </div>
                     @endisset
+                    <hr class="my-1">
+                    <p class="mb-0 pl-2"><i class="fas fa-sort-amount-up mr-1"></i>新しい順</p>
+                    <hr class="my-1">
+                    <p class="mb-0 pl-2 mb-2"><i class="fas fa-sort-amount-down mr-1"></i>古い順</p>
                 </div>
 
             </div>
@@ -59,20 +66,13 @@
         class="list-group-item list-group-item-action py-2 ripple"
         aria-current="true"
         >
-        <i class="far fa-plus-square fa-fw me-3 mr-2"></i><span>新規作成</span>
-        </a>
-
-        <a
-        href="#"
-        class="list-group-item list-group-item-action py-2 ripple"
-        >
-        <i class="fas fa-tag fa-fw me-3 mr-2"></i><span>タグ</span>
+        <i class="fas fa-plus-square me-3 mr-2"></i><span>新規作成</span>
         </a>
 
         <a
         href="{{ route('trash.index') }}"
         class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-trash fa-fw me-3 mr-2"></i><span>ゴミ箱</span></a
+        ><i class="fas fa-trash-alt me-3 mr-2"></i><span>ゴミ箱</span></a
         >
     </side-navbar>
 </header>
