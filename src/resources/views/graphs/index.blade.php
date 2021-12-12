aa20662ab3b8fe808b5aeb8c6a785ae1ada67240
naoki <hinnm483@gmail.com>
2021-12-13 03:19:13 +0900

    メモがない時の画面と処理を実装/#97

--- src/resources/views/graphs/index.blade.php
+++ src/resources/views/graphs/index.blade.php
@@ -14,9 +14,19 @@
 
         @slot('main_content')
 
-            @include('sub_views.graphs.index_base',
-                    ['graphs_select' => $graphs,
-                    'is_trash' => false])
+            @if($graphs->isEmpty())
+                <div class="w-100 d-flex align-items-center justify-content-center text-center" style="height: 90vmin">
+                    <div>
+                        <i class="fas fa-file-signature fa-9x mb-4" style="color: #BDBDBD"></i>
+                        <p class="mt-4 h3" style="color: #afaeae">メモがありません</p>
+                    </div>
+                </div>
+
+            @else
+                @include('sub_views.graphs.index_base',
+                        ['graphs_select' => $graphs,
+                        'is_trash' => false])
+            @endif
 
         @endslot
 
