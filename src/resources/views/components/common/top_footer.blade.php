823844b9b645a8118a81b6b0a68bd9b40dba0c0f
naoki <hinnm483@gmail.com>
2021-12-17 05:40:49 +0900

    認証系の遷移とblade更新/#102

---
+++ src/resources/views/components/common/top_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="page-footer font-small bg-success">
+    <div class="pt-3 footer-copyright text-center">
+        <a href="{{ route('top.show') }}" class="mr-4">ホーム</a>
+        <a href="https://twitter.com/hinanam3" class="mr-4">利用規約</a>
+        <a href="https://twitter.com/hinanam3">プライバシーポリシー</a>
+    </div>
+
+    <div class="pb-3 pt-2 footer-copyright text-center">Copyright © 2021 ：na
+        <a href="https://twitter.com/hinanam3"> @hinanam3</a>
+    </div>
+</footer>
